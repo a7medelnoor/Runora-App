b3cd426e1fad6ec01de88dd016a79ef7bbbddb44
ahmed <acc.ahmedstudio@gmail.com>
2023-05-03 01:36:06 +0400

    get data from firebase

--- app/src/main/java/com/runora_dev/runoraf/Activity/DailyReportActivity.java
+++ app/src/main/java/com/runora_dev/runoraf/Activity/DailyReportActivity.java
@@ -5,16 +5,30 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.runora_dev.runoraf.R;
 import com.runora_dev.runoraf.Webservice.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class DailyReportActivity extends AppCompatActivity {
@@ -22,7 +36,7 @@ public class DailyReportActivity extends AppCompatActivity {
     DatabaseHelper databaseHelper;
     String[] items;
     Button bclose;
-
+    private static final String TAG = "DailyReportActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,6 +74,56 @@ public class DailyReportActivity extends AppCompatActivity {
                     R.layout.support_simple_spinner_dropdown_item, items);
             l.setAdapter(arr);
         }
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("dailyFood");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Handle data change event
+                List<String> items = new ArrayList<>();
+                items.add("Breakfast : Calori - Lunch : Calori - Dinner : Calori");
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String str = "" + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    str = str + " - " + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    str = str + " - " + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    items.add(str);
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(DailyReportActivity.this,
+                        android.R.layout.simple_list_item_1, items);
+                l.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle database error
+            }
+        });
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Handle data change event
+                List<String> items = new ArrayList<>();
+                items.add("Breakfast : Calori - Lunch : Calori - Dinner : Calori");
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String str = "" + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    str = str + " - " + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    str = str + " - " + snapshot.child("food_name").getValue(String.class) + " : " + snapshot.child("calories").getValue(Float.class);
+                    items.add(str);
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(DailyReportActivity.this, android.R.layout.simple_list_item_1, items);
+                l.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle database error
+            }
+        });
+
+
     }
 
     public void onProfileClick(View v) {
